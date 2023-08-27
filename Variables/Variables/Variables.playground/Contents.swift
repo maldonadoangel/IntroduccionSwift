fe1b6c18a77e7f9038fe844d0590cb9b3f93e1db
Angel Morales <angelmorales@MacBook-Pro-de-Angel.local>
2023-08-27 15:26:38 -0600

    Curso de Swift

---
+++ Variables/Variables/Variables.playground/Contents.swift
@@ -0,0 +1,18 @@
+//Import sirve para importar librerias
+//Las librerias son lineas de codigo, funciones que podemos reutilizar
+import UIKit
+
+let saludoConstate: String = "Hola"
+var nombre: String = "Angel Morales"
+let edad: Int = 25
+let anios: Int = 10
+print(saludoConstate + " " + nombre + " tu edad es: " + String(edad))
+
+//Enteros
+print("En 10 años tu edad sera: ", 25 + 10)
+
+//Flotantes
+
+//String
+
+
