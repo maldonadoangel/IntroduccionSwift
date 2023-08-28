32e1d12f1b8d92b7a940558cd0b1c9cdb50aba60
Angel Morales <maldonadoangel7069@gmail.com>
2023-08-27 19:59:21 -0600

    Comparación y recorrido de Strings

---
+++ Strings/Strings.playground/Pages/ComparacionStrings.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Comparacion
+
+var nombre = "Angel"
+var nombre2 = "Hernan"
+var nombre3 = "Angel"
+
+
+//Distintos
+nombre == nombre2
+
+//Iguales
+nombre == nombre3
+
+
+
+//Contenido
+
+let miContenido = "Mi nombre es Angel"
+let contenidoVacio: String = ""
+miContenido.contains(nombre)
+
+
+//Contenido vacio
+
+print(contenidoVacio.isEmpty)
+
+print()
+//Recorrido
+for valor in miContenido{
+    
+    
+    print(valor)
+}
