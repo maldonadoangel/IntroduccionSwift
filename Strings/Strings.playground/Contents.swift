927ce800a545cdc9b8705c37f745ce98a9b2b788
Angel Morales <maldonadoangel7069@gmail.com>
2023-08-27 18:19:01 -0600

    String

---
+++ Strings/Strings.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var nombre: String = "Angel Hernán Moraes Maldonado"
+let bienvenida: String = "Bienvenido: "
+let mensajeCompleto: String = bienvenida + nombre
+
+let mensajeEnVariasLineas: String = """
+Buen día estimados,
+Espero se encuentren bien, este es un mensaje multilinea
+deberia de mostrar los mensajes de manera completa
+"""
+print(mensajeCompleto)
+print(mensajeEnVariasLineas)
